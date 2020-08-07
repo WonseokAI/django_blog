33a4c231293ffede7a5bb3bfa14b41a9d5a06e56
Wonseok <ws4423@gmail.com>
2020-08-07 23:47:25 +0900

    admin custom01

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from blog.models import Category, Post
 
 admin.site.register(Category)
-admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'createDate', 'updateDate']
+    list_filter = ['category']
+    search_fields = ['title']
+    ordering = ['-createDate', '-updateDate']
+
+
+admin.site.register(Post, PostAdmin)
